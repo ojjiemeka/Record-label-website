f18ce4380c8eeb3ca5b9b77fef8151d28aa7585b
ojjiemeka <63786841+ojjiemeka@users.noreply.github.com>
2022-08-05 03:01:50 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './pages/user/index/index.component';
+import { AboutComponent } from './pages/user/about/about.component';
+import { BlogComponent } from './pages/user/blog/blog.component';
+import { ContactComponent } from './pages/user/contact/contact.component';
+import { ArtistsComponent } from './pages/user/artists/artists.component';
+import { ArtistDetailComponent } from './pages/user/artist-detail/artist-detail.component';
+import { CareerComponent } from './pages/user/career/career.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    ArtistsComponent,
+    ArtistDetailComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,
